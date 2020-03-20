e3ad7154f3ba4b7d9c20c352151aaf5daa4104b5
PARK SUNG DONG <oraedoa@gmail.com>
2020-03-20 23:42:46 +0900

    init

---
+++ src/views/CustomDialog.vue
@@ -0,0 +1,36 @@
+<template>
+  <div id="customDialog">
+    <el-dialog title="Error Dialog Test By pasudo123" :visible.sync="computedIsOpen">
+        IE 에서는 다이얼로그가 처음 까맣게 나타난다.
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CustomDialog',
+  props: {
+    isOpen: {
+        type: Boolean,
+        required: true,
+        default: () => false
+    }
+  },
+  computed: {
+    computedIsOpen:{
+        get(){
+            return this.isOpen;
+        },
+        set(){
+            this.close();
+        }
+    }
+  },
+  methods:{
+      close(){
+          this.$emit('closeDialog');
+      }
+  }
+}
+</script>
---
+++ src/views/Index.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="index">
+    <button @click=openDialog>
+        Button
+    </button>
+    <div>
+        <custom-dialog 
+            v-bind:isOpen="isOpen"
+            v-on:closeDialog="closeDialog"/>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import CustomDialog from './CustomDialog';
+
+export default {
+  name: 'Index',
+  components: { CustomDialog },
+  data() {
+      return {
+          isOpen: false
+      }
+  },
+  methods: {
+      openDialog(){
+        this.isOpen = true;
+      },
+      closeDialog() {
+          this.isOpen = false;
+      }
+  }
+}
+</script>
